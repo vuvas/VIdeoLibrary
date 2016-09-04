df2800d7855a835f4c42393897e3f4c65ea0f4af
Yared Dejene <yareddej@gmail.com>
2016-09-04 17:52:42 +0300

    Remove Comments

--- src/app/video/videos.controller.js
+++ src/app/video/videos.controller.js
@@ -6,7 +6,6 @@ angular.module('myApp.controllers')
 function VideosController($scope, ApiService) {
     $scope.videos = [];
     var pageSize = 10, page = 0;
-    var previousPage = null;
     var inProgress = true;
 
     $scope.loadVideos = function () {
@@ -15,16 +14,10 @@ function VideosController($scope, ApiService) {
                 skip: page * pageSize,
                 limit: pageSize
             };
-            console.log('previousPage', previousPage);
-            if (page == previousPage) {
-                return false;
-            }
-            console.log(page, param);
             ApiService.GetObject(API.Video.Get.Videos, param).then(function (response) {
                 if (response.status == "success") {
                     //console.log("Response", response);
                     $scope.videos = $scope.videos.concat(response.data);
-                    previousPage = page;
                     page++;
                 } else {
                     inProgress = false;
