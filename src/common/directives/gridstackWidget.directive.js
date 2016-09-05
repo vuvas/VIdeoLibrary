75fbb93d64b92557ec4cbc9aa994149cbcb520e5
Yared Dejene <yareddej@gmail.com>
2016-09-05 11:03:29 +0300

    Added GridStack

---
+++ src/common/directives/gridstackWidget.directive.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Jared on Sep 05, 2016.
+ */
+angular.module('myApp.directives')
+    .directive('gridstatckWidget', function($rootScope, $window) {
+        return {
+            scope: {
+                gridstackOptions: '='
+            },
+            link: function(scope, element){
+                //Wait 1 sec DOM till has finished rendering
+                $timeout(function () {
+                    element.gridstack(scope.gridstackOptions);
+                }, 1000);
+            }
+        }
+    });
